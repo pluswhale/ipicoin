fde34586d38926638415e06b29ffbaaaa5a201ab
PashaShilik <pasha-shilik@mail.ru>
2023-05-25 16:12:57 +0300

    create user card

--- components/ContactUs/ContactUs.vue
+++ components/ContactUs/ContactUs.vue
@@ -1,47 +1,47 @@
 <template>
     <div class="flex flex-col items-center mt-[200px] justify-center">
-        <h2 class="text-white uppercase text-[40px] font-['Quicksand sans-serif'] mb-[9px]">Contact Us</h2>
+        <h2 class="text-white uppercase text-[40px] font-['Quicksand'] mb-[9px]">Contact Us</h2>
 
         <div class="max-w-[534px] mb-[38px]">
-            <p class="text-[#97AFD5] text-[16px] font-['Roboto sans-serif']">We are always open and we welcome and questions you have for our team. If you wish to get in touch, please fill out the form below. Someone from our team will get back to you shortly.</p>
+            <p class="text-[#97AFD5] text-[16px] font-[400] font-['Roboto']">We are always open and we welcome and questions you have for our team. If you wish to get in touch, please fill out the form below. Someone from our team will get back to you shortly.</p>
         </div>
 
         <div class="flex flex-col items-center w-[100%] mb-[84px]">
             <div class="flex w-[534px] items-center justify-between mb-[30px]" >
 
                 <div class="flex flex-col <!-- mr-[40px] -->">
-                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto sans-serif'] mb-[10px]">Your Name</p>
-                    <input type="text" placeholder="Introduce yourself" class="bg-[#FFFFFF]/10 rounded-[6px] py-[12px] px-[15px] outline-none text-white">
+                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto'] mb-[10px]">Your Name</p>
+                    <input type="text" placeholder="Introduce yourself" class="bg-[#FFFFFF]/10 font-[400] text=[14.4px] font-['Roboto sans-serif']  rounded-[6px] py-[12px] px-[15px] outline-none text-white">
                 </div>
 
                 <div class="flex flex-col ml-[5px]">
-                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto sans-serif'] mb-[10px]">Your Email</p>
+                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto'] mb-[10px]">Your Email</p>
                     <input type="text" placeholder="Who do we replay to" class="bg-[#FFFFFF]/10 rounded-[6px] py-[12px] px-[15px] outline-none text-white">
                 </div>
             </div>
 
             <div class="flex flex-col w-[534px] mb-[41px]">
-                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto sans-serif'] mb-[10px]">Your Message</p>
+                    <p class="text-[#97AFD5] uppercase text-[12px] font-['Roboto'] mb-[10px]">Your Message</p>
                     <textarea placeholder="Leave your question or comment here" class="w-[100%] h-[105px] bg-[#FFFFFF]/10 rounded-[6px] py-[10px] px-[15px] outline-none text-white resize-none"></textarea>
             </div>
 
             <div class="flex items-center rounded-[20px] py-[14px] px-[40px] bg-[#EC474E] cursor-pointer">
-                <button class="text-white uppercase text-[16px] font-['Barlow sans-serif'] mr-[2px]">send message</button>
+                <button class="text-white uppercase text-[16px] font-['Barlow'] mr-[2px]">send message</button>
                 <img src="../../assets/icons/Contact/arrow-right.png" alt="">
             </div>
 
             <div class="flex items-center w-{534px} justify-between mt-[85px]">
                 <div class="flex items-center cursor-pointer">
                     <img src="../../assets/icons/Contact/tel-logo.png" alt="">
-                    <p class="text-[#97AFD5] text-[15px] font-['Roboto sans-serif'] ml-[15px]">In progress...</p>
+                    <p class="text-[#97AFD5] text-[15px] font-['Roboto'] ml-[15px]">In progress...</p>
                 </div>
                 <div class="flex items-center mx-[20px] cursor-pointer">
                     <img src="../../assets/icons/Contact/mail-logo.png" alt="">
-                    <p class="text-[#97AFD5] text-[15px] font-['Roboto sans-serif'] ml-[15px]">In progress...</p>
+                    <p class="text-[#97AFD5] text-[15px] font-['Roboto'] ml-[15px]">In progress...</p>
                 </div>
                 <div class="flex items-center cursor-pointer">
                     <img src="../../assets/icons/Contact/telegram-logo.png" alt="">
-                    <p class="text-[#97AFD5] text-[15px] font-['Roboto sans-serif'] ml-[15px]">In progress...</p>
+                    <p class="text-[#97AFD5] text-[15px] font-['Roboto'] ml-[15px]">In progress...</p>
                 </div>
 
             </div>
--- components/UserCard/UserCard.vue
+++ components/UserCard/UserCard.vue
@@ -1,32 +1,111 @@
 <template>
     <div class=" flex flex-col items-center w-[100%]">
-        
-        <div class="flex items-center w-[100%] mb-[40px]">
-            <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5">
 
+        <div class="flex items-center w-[100%] mb-[40px]">
+            <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5 relative">
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-1.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Emmanuel Rodes</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
 
             <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5 mx-[20px]">
-
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-2.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Emmanuel Rodes</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
 
             <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5">
-
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-3.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Ninel Adarman</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
         </div>
 
         <div class="flex items-center w-[100%] mb-[40px]">
             <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5">
-
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-4.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Emmanuel Rodes</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
 
             <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5 mx-[20px]">
-
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-5.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Ninel Adarman</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
 
             <div class="flex flex-col w-[453px] px-[18px] py-[17px] min-h-[161px] bg-[#FFFFFF]/5">
-
+                <div class="flex items-center mb-[18px]">
+                    <img src="../../assets/icons/CardUser/User-6.png" alt="User">
+                    <div class="flex flex-col ml-[16px]">
+                        <p class="font-400 text-[14px] font-['Roboto'] text-white">Emmanuel Rodes</p>
+                        <p class="font-400 text-[12px] font-['Roboto'] text-[#838A91]">20 Mar 2023 at 05:15 PM</p>
+                    </div>
+                    <img src="../../assets/icons/CardUser/twit.png" alt="ico" class="absolute top-[20px] right-[20px]">
+                </div>
+                <p class="font-400 text-[15px] font-['Roboto'] text-white">Thanks @ipicoin for your guidelines. I agree with you.<br> @Dun can you implement the way Ipicoin has described ?</p>
+                <div class="flex items-end w-[100%] justify-end mt-[10px]">
+                    <a href="/" class="font-400 text-[12px] font-['Gilroy-Light, sans-serif'] text-[#97AFD5] border-b border-[#97AFD5]">Reply</a>
+                    <img class="ml-[17px]" src="../../assets/icons/CardUser/heart.svg" alt="ico">
+                </div>
             </div>
+        </div>
+
+        <div class=" flex items-center mb-[200px]">
+            <div class="rounded-[100%] h-[24px] w-[24px] border-[1px] border-[#97AFD5]/10 mr-[12px] cursor-pointer"></div>
+            <div class="rounded-[100%] h-[24px] w-[24px] border-[1px] border-[#97AFD5]/10 mr-[12px] cursor-pointer"></div>
+            <div class="rounded-[100%] h-[24px] w-[24px] border-[1px] border-[#97AFD5]/10 mr-[12px] cursor-pointer"></div>
+            <div class="rounded-[100%] h-[24px] w-[24px] border-[3px] border-[#97AFD5] cursor-pointer"></div>
         </div>
 
     </div>
