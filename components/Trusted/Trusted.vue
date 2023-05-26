f4284df66195887ce5b8cc58d025e7059bef84bb
PashaShilik <pasha-shilik@mail.ru>
2023-05-26 16:41:38 +0300

    create trust

--- components/Trusted/Trusted.vue
+++ components/Trusted/Trusted.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="flex flex-col items-center mt-[144px] max-w-[1360px] mx-auto my-0">
         <h2 class="text-white uppercase text-[40px] font-[700] font-['Quicksand'] mb-[50px]">trusted by the best</h2>
-        <div class="flex flex-col items-center flex-wrap w-[110%] mb-[50px]">
-            <div class="flex justify-center items-center w-[110%] mb-[30px]" >
+        <div class="flex flex-col items-center flex-wrap w-[100%] mb-[50px]">
+            <div class="flex justify-center items-center w-[100%] mb-[30px]" >
                 <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px] ">
                     <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
                 </div>
@@ -24,7 +24,7 @@
                 </div>
             </div>
 
-            <div class="flex justify-center items-center w-[110%] mb-[0]" >
+            <div class="flex justify-center items-center w-[100%] mb-[0]" >
                 <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px]">
                     <img src="../../assets/icons/Trusted/META_logo.png" alt="">
                 </div>
