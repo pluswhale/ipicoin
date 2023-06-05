230b3a3de29c3a6aaa631c1dafe1c3671560d2e6
PashaShilik <pasha-shilik@mail.ru>
2023-06-05 17:41:42 +0300

    create tablet arrow

--- components/Ecosystem/Ecosystem.vue
+++ components/Ecosystem/Ecosystem.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex flex-col items-center w-full mt-[140px] max-w-[1400px] mx-auto my-0 translate-x-[-10px]" >
-        <h2 class="text-white uppercase text-[40px] font-[700] font-['Quicksand'] mb-[74px]">ecosystem</h2>
+        <h2 class="text-white uppercase text-[40px] font-[700] font-['Quicksand'] mb-[42px]">ecosystem</h2>
         <div class="flex flex-col max-w-[1068px] justify-between">
             <!--top part-->
-            <div class="flex flex-row max-w-[1068px] justify-between translate-x-[-30px]">
+            <div class="flex flex-row max-w-[1068px] justify-between translate-x-[-65px]">
                 <!--first arrows-->
                 <div class="flex flex-col gap-[33px]">
-                    <img class="translate-y-[0]" src="~/assets/images/ecosystem/first_col_arrow_1.svg"/>
-                    <img class="translate-y-[0]" src="~/assets/images/ecosystem/first_col_arrow_2.svg"/>
+                    <img class="translate-y-[20px] translate-x-[257px] absolute w-[214px]" src="../../assets/images/platform/arrow1.svg" alt=""/>
+                    <img class="translate-y-[20px] translate-x-[70px] h-[220px] absolute" src="../../assets/images/platform/arrow2.svg" alt=""/>
                 </div>
                 <!--first arrows-->
 
@@ -20,24 +20,24 @@
                             <div class="flex flex-col">
                                 <!-- little card-->
                                 <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[50px] translate-y-[0] justify-center items-center rounded-[20px]" >
-                                    <span class="font-['Quicksand'] text-[16px] font-[500] text-white text-center">iScan</span>
+                                    <span class="font-['Quicksand'] text-[16px] font-[500] text-white text-center relative">iScan</span>
                                 </div>
                                 <!-- little card-->
 
                                 <!-- little card-->
-                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[50px] translate-y-[30px] justify-center items-center rounded-[20px]" >
+                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[50px] translate-y-[43px] justify-center items-center rounded-[20px]" >
                                     <span class="font-['Quicksand'] text-[14px] font-[500] text-white text-center ">iWallet</span>
                                 </div>
                                 <!-- little card-->
 
                                 <!-- little card-->
-                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[70px] translate-y-[63px] justify-center items-center rounded-[20px]" >
+                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[70px] translate-y-[93px] justify-center items-center rounded-[20px]" >
                                     <span class="font-['Quicksand'] text-[14px] font-[500] text-white  text-center">index Real-time Indexer</span>
                                 </div>
                                 <!-- little card-->
 
                                 <!-- little card-->
-                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[70px] justify-center translate-y-[90px] items-center rounded-[20px]" >
+                                <div class="flex bg-[#9B51E01A]/10 w-[154px] h-[70px] justify-center translate-y-[117px] items-center rounded-[20px]" >
                                     <span class="font-['Quicksand'] text-[14px] font-[500] text-white max-w-[179px] text-center">Solidity<br> to iVM Transpiler</span>
                                 </div>
                                 <!-- little card-->
@@ -46,23 +46,24 @@
 
                             <!-- second arrow right first block-->
                             <div class="relative">
-                                <img class="translate-y-[40px] translate-x-[5px]" src="../../assets/images/platform/arrow1.png" alt=""/>
-                                <img class="translate-y-[-130px] h-[170px] translate-x-[-180px]" src="../../assets/images/platform/arrow2.png" alt=""/>
-                                <img class="translate-y-[-35px] translate-x-[-190px] h-[450px] absolute" src="../../assets/images/platform/arrow3.svg" alt=""/>
+                                <!--<img class="translate-y-[40px] translate-x-[5px] absolute " src="../../assets/images/platform/arrow1.png" alt=""/> -->
+                                <!--<img class="translate-y-[-130px] h-[170px] translate-x-[-180px] absolute" src="../../assets/images/platform/arrow.svg" alt=""/> -->
+                                <!-- <img class="translate-y-[-35px] translate-x-[-190px] absolute" src="../../assets/images/platform/arrow.svg" alt=""/> -->
                             </div>
 
                         </div>
                         <!--first 4 card and arrows-->
 
                         <!-- big card-->
-                        <div class="flex flex-col bg-[#9B51E01A]/10 w-[190px] h-[157px] translate-y-[170px] justify-center items-center rounded-[20px]" >
-                            <span class="font-['Quicksand'] text-[14px] font-[700] text-white  text-center">Simple Transactions</span>
+                        <div class="flex flex-col bg-[#9B51E01A]/10 w-[190px] h-[157px] translate-y-[236px] justify-center items-center rounded-[20px] relative" >
+                            <span class="font-['Quicksand'] text-[14px] font-[700] text-white text-center translate-y-[-7px]">Simple Transactions</span>
                             <div class="flex flex-col gap-[9px] mt-[7px]">
                                 <span class="flex items-center justify-center w-[149px] h-[28px] bg-[#749CC799]/60 rounded-[20px] text-white text-['Quicksand'] text-[14px] font-[500]">Transfer</span>
                                 <span class="flex items-center justify-center w-[149px] h-[28px] bg-[#749CC799]/60 rounded-[20px] text-white text-['Quicksand'] text-[14px] font-[500]">Creat Asset</span>
                                 <span class="flex items-center justify-center w-[149px] h-[28px] bg-[#749CC799]/60 rounded-[20px] text-white text-['Quicksand'] text-[14px] font-[500]">Create Contract</span>
                             </div>
-                            <img src="../../assets/images/platform/arrow4.png" class="translate-y-[35px]" alt="">
+                            <img src="../../assets/images/platform/arrow3.svg" class="translate-y-[-30px] translate-x-[-113px] absolute" alt="">
+                            <img src="../../assets/images/platform/arrow4.svg" class="translate-y-[96px] translate-x-[0px] absolute" alt="">
                         </div>
                         <!-- big card-->
                     </div>
@@ -72,7 +73,7 @@
                     <div class="flex w-[965px]">
 
                         <!-- main block wrapper-->
-                        <div class="flex flex-col w-[628px] h-[747px] translate-x-[107px]">
+                        <div class="flex flex-col w-[628px] h-[747px] translate-x-[285px] translate-y-[-5px]">
                             <!--first card -->
                             <div class=" flex flex-col bg-[#FFFFFF33] items-center justify-center rounded-[20px] w-full min-h-[83px] mb-[15px]">
                                 <span class="text-[#EC474E] text-[700] font-['Quicksand'] mb-[10px] text-[14px] font-[700] mt-[10px]">Web application</span>
@@ -85,7 +86,7 @@
                             <!--first card -->
 
                             <!--second card -->
-                            <div class=" flex flex-col bg-[#FFFFFF33] items-center justify-center rounded-[20px] w-full h-[87px] mb-[15px]">
+                            <div class=" flex flex-col bg-[#FFFFFF33] items-center justify-center rounded-[20px] w-full h-[87px] mb-[15px] relative">
                                 <span class="text-[#EC474E] text-[14px] font-['Quicksand'] mb-[10px] font-[700]">Web3 Protocols</span>
                                 <div class="flex items-center gap-[8px]">
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[16px] pl-[16px] pr-[20px] h-[33px] font-[500] text-[14px]">iSwap</span>
@@ -95,6 +96,8 @@
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[16px] pl-[16px] pr-[20px] h-[33px] font-[500] text-[14px]">iGamble</span>
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[16px] pl-[16px] pr-[20px] h-[33px] font-[500] text-[14px]">iFund</span>
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[16px] pl-[16px] pr-[20px] h-[33px] font-[500] text-[14px]">iDAO</span>
+                                    
+                                    <img src="../../assets/images/platform/arrow9(1).svg" class="translate-y-[75px] translate-x-[27px] right-0 absolute" alt="">
                                 </div>                            
                             </div>
                             <!--second card -->
@@ -123,11 +126,11 @@
                                 <div class="flex w-[148px] items-center justify-center bg-[#FFFFFF33] rounded-[20px] h-full text-white text-center ">
                                    <p class="text-white text-[14px] font-[500] font-['Quicksand'] leading-[20px] w-[100%]">Low level and transpiled contracts</p> 
                                 </div>
-                                <div class="flex flex-col w-[470px] items-center bg-[#FFFFFF33] rounded-[20px] h-full text-white text-[14px] font-[500] font-['Quicksand'] ">
+                                <div class="flex flex-col w-[475px] items-center bg-[#FFFFFF33] rounded-[20px] h-full text-white text-[14px] font-[500] font-['Quicksand'] ">
                                     <div class="flex items-center gap-[15px] mt-[10px]">
                                         <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[111px] h-[33px] text-[14px] font-[500] font-['Quicksand'] pb-[3px]">Asyncify</span>
                                         <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[111px] h-[33px] text-[14px] font-[500] font-['Quicksand'] pb-[3px]">Optimizer</span>
-                                        <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[200px] h-[33px] text-[14px] font-[500] font-['Quicksand'] pb-[3px]">iRPC definitions extraction</span>
+                                        <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[205px] h-[33px] text-[14px] font-[500] font-['Quicksand'] pb-[3px]">iRPC definitions extraction</span>
                                     </div> 
                                     <span class="text-[#EC474E] text-[14px] font-[700] font-['Quicksand'] mt-[3px] mb-[3px]">iWASM Transpiler</span>
                                 </div>
@@ -135,29 +138,30 @@
                             <!--fourth card -->
 
                             <!--five card -->
-                            <div class=" flex flex-col mb-[15px] items-center bg-[#FFFFFF33] rounded-[20px] w-full h-[164px] relative">
+                            <div class=" flex flex-col mb-[15px] items-center bg-[#FFFFFF33] rounded-[20px] w-full h-[151px] relative">
                                 <span class="text-[#EC474E] text-[14px] font-[700] font-['Quicksand'] mt-[6px] mb-[8px]">iVM</span>
-                                <div class="flex bg-white/20 rounded-[20px] h-[44px] w-[559px] justify-center items-center mb-[3px] px-[20px]">
+                                <div class="flex bg-white/20 rounded-[20px] h-[44px] w-[559px] justify-center items-center mb-[5px] px-[20px]">
                                     <span class="text-[14px] text-center text-white font-['Quicksand'] font-[500] w-[100px] leading-[15px] my-[10px]">iWASM Transpiler</span>
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[132px] mr-[11px] h-[25px] font-[500] text-[14px]">Gas Metering</span>
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[132px] mr-[11px] h-[25px] font-[500] text-[14px]">SoftFloat</span>
                                     <span class="flex items-center justify-center text-white bg-[#749CC799]/60 rounded-[20px] w-[154px] h-[25px] font-[500] text-[14px]">SoftFloat</span>
                                 </div>
-                                <div class="flex bg-white/20  rounded-[20px] h-[33px] w-[559px] justify-center items-center mb-[3px]">
+                                <div class="flex bg-white/20 rounded-[20px] h-[29px] w-[559px] justify-center items-center mb-[3px]">
                                     <span class="text-white font-['Quicksand'] font-[500] text-[14px] text-center">WAVM -Fastest WebAssembly Virtual Machine</span>
                                 </div>
 
-                                <div class="flex bg-white/20  rounded-[20px] h-[33px] w-[559px] justify-center items-center">
+                                <div class="flex bg-white/20  rounded-[20px] h-[28px] w-[559px] justify-center items-center">
                                     <span class=" text-white font-['Quicksand'] font-[500] text-[14px] text-centr">Contract linking and communitation layer</span>
                                 </div>
+
+                                <img src="../../assets/images/platform/arrow8.png" class="translate-y-[132px] translate-x-[-400px]  absolute" alt="">
                             </div>
                             <!--five card -->
-                    <!--         <img src="../../assets/images/platform/arrow8.png" class="translate-y-[0px] translate-x-[100px]  absolute" alt=""> -->
+                            
                         </div>
                         <!-- main block wrapper-->
 
                         <!--right arrow-->
-                     <img src="../../assets/images/platform/arrow9.png" class="translate-y-[50px] translate-x-[100px] h-[370px]" alt="">
                         <!--right arrow-->
                     </div>
                     <!--top right block-->
@@ -176,21 +180,21 @@
                     <img class="translate-y-[-76px] translate-x-[200px]" src="~/assets/images/ecosystem/bottom_arrows.svg"/>
                     <img class="translate-y-[-76px] translate-x-[270px]" src="~/assets/images/ecosystem/bottom_arrow_white_up.svg"/>
                     <img class="translate-y-[-76px] translate-x-[550px]" src="~/assets/images/ecosystem/bottom_arrow_two.svg"/> -->
-                    <img src="../../assets/images/platform/arrow5.png" class="translate-y-[-185px] translate-x-[215px] absolute" alt="">
-                    <img src="../../assets/images/platform/arrow6.svg" class="translate-y-[-195px] translate-x-[910px] h-[35px]" alt="">
-                    <img src="../../assets/images/platform/arrow6.svg" class="translate-y-[-195px] translate-x-[425px] h-[35px]" alt="">
-                    <img src="../../assets/images/platform/arrow7.svg" class="translate-y-[-195px] translate-x-[665px] h-[35px]" alt="">
+                    <img src="../../assets/images/platform/arrow5.png" class="translate-y-[-205px] translate-x-[160px] absolute" alt="">
+                    <img src="../../assets/images/platform/arrow6(1).svg" class="translate-y-[-215px] translate-x-[890px] h-[35px]" alt="">
+                    <img src="../../assets/images/platform/arrow6(1).svg" class="translate-y-[-215px] translate-x-[450px] h-[35px]" alt="">
+                    <img src="../../assets/images/platform/arrow7(1).svg" class="translate-y-[-215px] translate-x-[620px] " alt="">
                 </div>
                 <!--bottom arrow-->
                 
                 <!--bottom block-->
-                <div class="flex bg-[#749CC733]/20 rounded-[20px] h-[60px] translate-x-[80px] translate-y-[20px] justify-center relative w-[940px]">
+                <div class="flex bg-[#749CC733]/20 rounded-[20px] h-[60px] translate-x-[30px] translate-y-[-5px] justify-center relative w-[1005px]">
                     <div class="flex w-[95%] items-center justify-between gap-[24px] translate-y-[-30px]">
                         <span class="flex items-center justify-center text-white bg-[#749CC7] rounded-[20px] w-[219px] h-[33px] font-['Quicksand'] font-[500] text-[14px]">Accounts</span>
                         <span class="flex items-center justify-center text-white bg-[#749CC7] rounded-[20px] w-[219px] h-[33px] font-['Quicksand'] font-[500] text-[14px]">Assets</span>
                         <span class="flex items-center justify-center text-white bg-[#749CC7] rounded-[20px] w-[219px] h-[33px] font-['Quicksand'] font-[500] text-[14px]">Code</span>
                         <span class="flex items-center justify-center text-white bg-[#749CC7] rounded-[20px] w-[219px] h-[33px] font-['Quicksand'] font-[500] text-[14px]">Key-Vaylue Data</span>
-                        <span class="absolute text-[#EC474E] top-[55px] left-[44%] font-['Quicksand'] font-[700] text-[14px]">Ledger Storage</span>
+                        <span class="absolute text-[#EC474E] top-[55px] left-[45.4%] font-['Quicksand'] font-[700] text-[14px]">Ledger Storage</span>
                     </div>
                 </div>
             </div>
--- components/MainPageSection/elements/GraphicBlock/GraphicBlock.vue
+++ components/MainPageSection/elements/GraphicBlock/GraphicBlock.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
         <div class="flex h-full flex-col justify-end translate-y-[0px] translate-x-[-50px]">
-            <img src="~/assets/images/diagram_main_page.png" class=""/>
+            <img src="~/assets/images/mainpage/Graph/x-info.png" class="relative"/>
+            <img src="~/assets/images/mainpage/Graph/bg.png" class="absolute bottom-[18px] w-[380px] right-0 translate-x-[-16px] "/>
+            <img src="~/assets/images/mainpage/Graph/white-line.png" class="absolute right-0 translate-x-[-16px] w-[260px] translate-y-[-42px]">
+            <img src="~/assets/images/mainpage/Graph/gray-line.png" class="absolute translate-x-[7px] w-[120px] translate-y-[-17px]">
+            <img src="~/assets/images/mainpage/Graph/dots.svg" class="absolute translate-x-[113px] translate-y-[-57px]">
+
+            <div class="absolute translate-x-[132px] translate-y-[-50px]">
+                <img src="~/assets/images/mainpage/Graph/statistick-window.png" class="relative">
+                <div class="absolute top-0 left-[17px]">
+                    <p class="font-['Cairo'] font-[700] text-[15px] translate-y-[5px]">0,3$</p>
+                    <p class="font-['Barlow'] font-[300] text-[14px]">current price</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
