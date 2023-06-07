ca3ae3820bc7d53da52ba5ff7bd6e1d0b5a0b7bd
Egor Dultsev <listenmy@yandex.ru>
2023-06-07 13:16:24 +0300

    add transition to our mission

--- components/OurMission/OurMission.vue
+++ components/OurMission/OurMission.vue
@@ -1,5 +1,42 @@
 <template>
-    <div v-motion-slide-visible-top class="flex flex-col items-center mt-[100px] h-[400px] max-w-[1400px] mx-auto my-0">
+    <div
+        v-motion
+        :initial="{
+            opacity: 0,
+            y: 100,
+            duration: 1000,
+            type: 'keyframes',
+            ease: 'easeIn',
+        }"
+        :enter="{
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 1000,
+                type: 'keyframes',
+                ease: 'easeIn',
+            },
+        }"
+        :visible="{
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 1000,
+                type: 'keyframes',
+                ease: 'easeIn',
+
+            },
+        }"
+        :leave="{
+           y: -100,
+            opacity: 0,
+             transition: {
+                delay: 1000,
+            },
+        }"
+      
+        class="flex flex-col items-center mt-[100px] h-[400px] max-w-[1400px] mx-auto my-0"
+    >
         <h2 class="text-white uppercase text-[40px] font-[700] font-['Quicksand'] mb-[50px]">our mission</h2>
 
         <div class="flex items-center flex-wrap w-[100%] mb-[50px] h-[100%] ">
