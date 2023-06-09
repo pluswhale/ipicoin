e6e07eb34b1c723f0fb888bf3a016a495ff11b8a
PashaShilik <pasha-shilik@mail.ru>
2023-06-09 13:15:37 +0300

    create button

--- components/OurAdvantagesChart/OurAdvantagesChart.vue
+++ components/OurAdvantagesChart/OurAdvantagesChart.vue
@@ -1,6 +1,6 @@
 <template>
     <div 
-        class="flex flex-col items-center w-full mt-[107px] max-w-[1400px] mx-auto my-0"
+        class="flex flex-col items-center w-full mt-[107px] max-w-[1400px] h-[1800px] mx-auto my-0"
      >
         <h2 class="text-white uppercase text-[40px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[15px] translate-x-[-14px]">our advantages</h2>
         <div class="flex w-[789px] border-b-[4px] border-b-[#749DCA] h-[100px] justify-center mb-[61px]">
