d91a9b19f575fb3d025bf619edbf24e9500a3be8
Egor Dultsev <listenmy@yandex.ru>
2023-06-16 15:29:31 +0300

    emoidijds

--- components/Trusted/Trusted.vue
+++ components/Trusted/Trusted.vue
@@ -36,7 +36,10 @@
         },
     }"   
    >
-        <h2 class="text-white uppercase text-[40px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[50px]">trusted by the best</h2>
+        <h2 class="text-white uppercase font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[50px]" :class="{
+            'text-[40px]': !isMobile,
+            'text-[22px]': isMobile
+        }">trusted by the best</h2>
         <div class="flex flex-col items-center flex-wrap w-[100%] mb-[50px]">
             <Vue3Marquee :pauseOnHover="true" :duration="70">
                 <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px] ">
@@ -78,55 +81,13 @@
                     <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
                 </div>
             </Vue3Marquee>
-            <!-- <div class="flex justify-center items-center w-[100%] mb-[30px]" >
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px] ">
-                    <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px]">
-                    <img src="../../assets/icons/Trusted/META_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px] ">
-                    <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px]">
-                    <img src="../../assets/icons/Trusted/META_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[60px] py-[10px] px-[31px] rounded-[8px] ">
-                    <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
-                </div>
-            </div> -->
-
-            <!-- <div class="flex justify-center items-center w-[100%] mb-[0]" >
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px]">
-                    <img src="../../assets/icons/Trusted/META_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px] ">
-                    <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px]">
-                    <img src="../../assets/icons/Trusted/META_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px] ">
-                    <img src="../../assets/icons/Trusted/Binance_logo.png" alt="">
-                </div>
-
-                <div class="flex flex-col justify-center items-center h-[70px] w-236px bg-[#FFFFFF]/10 mr-[64px] py-[10px] px-[31px] rounded-[8px]">
-                    <img src="../../assets/icons/Trusted/META_logo.png" alt="">
-                </div>
-            </div> -->
         </div>
    </div>
 </template>
 
 <script setup>
 import { Vue3Marquee } from 'vue3-marquee'
+const { isMobile } = useDevice();
 
 </script>
 
---
+++ componentsForMobile/IncrisingTrustForMobile/IncrisingTrustForMobile.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex  justify-center items-center w-screen self-start bg-white/10 h-[179px] ">
+        <div class="flex flex-col w-full items-center justify-between gap-[20px]">
+            <div class="flex gap-[40px]">
+                <div class="flex flex-col justify-center ">
+                    <span class="font-[500] font-['Inter'] text-[36px] text-[#749DCA] text-center textLine">40 k +</span>
+                    <span class="font-['Roboto'] text-[12px] font-[400] text-white ">active traders</span>
+                </div>
+
+
+                <div class="flex flex-col justify-center">
+                    <span class="font-[500] font-['Inter'] text-[36px] text-[#749DCA] text-center textLine">70 k +</span>
+                    <span class="font-['Inter'] text-[12px] font-[400] text-white ">Telegram subscribers</span>
+                </div> 
+            </div>
+
+
+            <div class="flex gap-[40px]">
+                <div class="flex flex-col justify-center">
+                    <span class="font-[500] font-['Inter'] text-[36px] text-[#749DCA] text-center textLine">10 k +</span>
+                    <span class="font-['Inter'] text-[12px] font-[400] text-white ">Registered user</span>
+                </div>
+
+
+                <div class="flex flex-col justify-center">
+                    <span class="font-[500] font-['Inter'] text-[36px] text-[#749DCA] text-center textLine"> &#60 0, 10 %</span>
+                    <span class="font-['Inter'] text-[12px] font-[400] text-white ">the lowest transaction fees</span>
+                </div>
+            </div>
+
+        
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.textLine{
+    text-shadow: 0px 0px 10px rgba(151, 175, 213, 0.9);
+}
+</style>
