6f29a8e47c46aa782b77f05529cd6efcb047d762
Egor Dultsev <listenmy@yandex.ru>
2023-06-23 11:41:34 +0300

    fix header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { isMobile } = useDevice();
+const { isMobile, isTablet } = useDevice();
 
 const stateMenu = reactive({ toDialogOpenIPI: false })
 
@@ -13,39 +13,39 @@ const handleOpenBurger = () => {
     <header 
         class="flex w-screen justify-center items-center shadow-sm bg-white/5 backdrop-blur-sm z-[1000]"
         :class="{
-            'h-[43px]': isMobile,
-            'h-[93px]': !isMobile,
-            'pl-[16px]': isMobile,
-            'pr-[16px]': isMobile,
-            'fixed': isMobile,
-            'relative': !isMobile,
-            'top-[0]': isMobile,
-            'left-[0]': isMobile,
+            'h-[43px]': isMobile || isTablet,
+            'h-[93px]': !isMobile && !isTablet,
+            'pl-[16px]': isMobile || isTablet,
+            'pr-[16px]': isMobile || isTablet,
+            'fixed': isMobile || isTablet,
+            'relative': !isMobile && !isTablet,
+            'top-[0]': isMobile || isTablet,
+            'left-[0]': isMobile || isTablet,
         }"
     >
             <div class="flex w-[1400px] justify-between items-center">
                 <div class="flex items-center">
                     <img 
                         :class="{
-                            'hidden' : isMobile,
-                            'h-[20px]': isMobile,
-                            'w-[66px]': isMobile,
+                            'hidden' : isMobile || isTablet,
+                            'h-[20px]': isMobile || isTablet,
+                            'w-[66px]': isMobile || isTablet,
                         }"
                         src="~/assets/images/ipi_logo_1.svg"
     
                     >
                     <img 
                         :class="{
-                            'ml-[1px]': isMobile, 
-                            'h-[20px]': isMobile,
-                            'w-[66px]': isMobile,
+                            'ml-[1px]': isMobile || isTablet, 
+                            'h-[20px]': isMobile || isTablet,
+                            'w-[66px]': isMobile || isTablet,
                         }"
                         class="max-h-[43px] ml-[11px]" 
                         src="~/assets/images/ipi_logo.svg"
     
                     >
                 </div>
-                <nav v-show="!isMobile" class="flex justify-between  translate-x-[-30px] min-w-[395px] ">
+                <nav v-show="!isMobile || !isTablet" class="flex justify-between  translate-x-[-30px] min-w-[395px] ">
                     <ul class="flex gap-[1.85rem]">
                         <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="#platform">Platform</NuxtLink></li>
                         <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="/">Coins</NuxtLink></li>
@@ -58,29 +58,29 @@ const handleOpenBurger = () => {
                 <button 
                     class="flex pr-[10px] pl-[10px] items-center justify-center transition-all border-[#EC474E]  font-['Barlow sans-serif'] hover:bg-[#EC474E] button_coin"
                     :class="{
-                        'rounded-[20px]': !isMobile,
-                        'rounded-[8px]': isMobile,
-                        'pl-[10px]': isMobile,
-                        'pr-[10px]': isMobile,
-                        'border-[2px]': !isMobile,
-                        'border-[1px]': isMobile,
-                        'h-[25px]': isMobile,
-                        'h-44px': !isMobile,
-                        'p-[0]': isMobile,
-                        'translate-x-[10px]': !isMobile,
-                        'translate-x-[-6px]': isMobile,
+                        'rounded-[20px]': !isMobile || !isTablet,
+                        'rounded-[8px]': isMobile || isTablet,
+                        'pl-[10px]': isMobile || isTablet,
+                        'pr-[10px]': isMobile || isTablet,
+                        'border-[2px]': !isMobile && !isTablet,
+                        'border-[1px]': isMobile || isTablet,
+                        'h-[25px]': isMobile || isTablet,
+                        'h-44px': !isMobile || !isTablet,
+                        'p-[0]': isMobile || isTablet ,
+                        'translate-x-[10px]': !isMobile && !isTablet,
+                        'translate-x-[-6px]': isMobile || isTablet,
                     }"
                 >
                     <span class=" font-[500] uppercase whitespace-nowrap text-white font-['Inter'] mr-[5px]"
                         :class="{
-                            'text-[10px]': isMobile,
-                            'text-[16px]': !isMobile,
+                            'text-[10px]': isMobile || isTablet,
+                            'text-[16px]': !isMobile && !isTablet,
                         }">Follow the ipicoin</span>
-                    <img :class="{'w-[20px]': isMobile}" src="~/assets/icons/arrow-right.svg" class="no_hover_arrow"/>
-                    <img :class="{'w-[20px]': isMobile}" src="~/assets/images/IpiScan/arrow-right.png" class="hover_arrow"/>
+                    <img :class="{'w-[20px]': isMobile || isTablet}" src="~/assets/icons/arrow-right.svg" class="no_hover_arrow"/>
+                    <img :class="{'w-[20px]': isMobile || isTablet}" src="~/assets/images/IpiScan/arrow-right.png" class="hover_arrow"/>
                 </button>
 
-                <img v-show="isMobile" @click="handleOpenBurger" src="~/assets/icons/mobile/burger_menu.svg" class="relative z-[6]"/>
+                <img v-show="isMobile || isTablet" @click="handleOpenBurger" src="~/assets/icons/mobile/burger_menu.svg" class="relative z-[6]"/>
                     <div v-if="stateMenu.toOpenBurger" class="flex flex-col items-center absolute w-full left-[0] top-[0px] h-screen bg-[#1C0D30]">
                         <div class="flex flex-col items-center h-[615px]">
 
@@ -102,9 +102,6 @@ const handleOpenBurger = () => {
                                 <p class="text-[13px] text-white font-[600] font-['Inter'] uppercase text-center mr-[5px]">Follow the iPIcoin</p>
                                 <img src="~/assets/icons/arrow-right.svg">
                             </button>
-
-
-
                         </div>
                     </div>
             </div>
