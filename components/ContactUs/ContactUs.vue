dea090ef63f9f507d8de01c18fb98ed0ac625a6f
Egor Dultsev <listenmy@yandex.ru>
2023-06-26 23:33:00 +0300

    add rdw to sections

--- components/ContactUs/ContactUs.vue
+++ components/ContactUs/ContactUs.vue
@@ -37,23 +37,23 @@
         class="flex flex-col items-center mt-[50px] justify-center  mx-auto my-0 max-[1200px]:max-w-[540px]"
     >
 
-        <h2 class="text-white uppercase text-[40px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[9px] max-[1200px]:text-[26px] max-[450px]:text-[26px]">Contact Us</h2>
+        <h2 class="text-white uppercase text-[40px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[9px] max-[1200px]:text-[26px] max-[910px]:text-[26px]">Contact Us</h2>
 
-        <div class="max-w-[540px] mb-[38px] max-[450px]:w-[343px]">
-            <p class="text-[#97AFD5] text-[16px] font-[400] font-['Roboto'] text-center leading-[26px] max-[1420px]:font-['Inter'] max-[1200px]:text-[14px] max-[450px]:text-[12px] ">We are always open and we welcome and questions you have for our team. If you wish to get in touch, please fill out the form below. Someone from our team will get back to you shortly.</p>
+        <div class="max-w-[540px] mb-[38px] max-[910px]:w-[343px]">
+            <p class="text-[#97AFD5] text-[16px] font-[400] font-['Roboto'] text-center leading-[26px] max-[1420px]:font-['Inter'] max-[1200px]:text-[14px] max-[910px]:text-[12px] ">We are always open and we welcome and questions you have for our team. If you wish to get in touch, please fill out the form below. Someone from our team will get back to you shortly.</p>
         </div>
 
-        <div class="flex flex-col items-center mb-[84px] max-[1200px]:w-[540px] max-[450px]:w-[343px]">
+        <div class="flex flex-col items-center mb-[84px] max-[1200px]:w-[540px] max-[910px]:w-[343px]">
 
-            <div class="flex w-[540px] items-center justify-between mb-[30px] max-[1420px]:w-full max-[450px]:flex-col max-[1420px]:gap-[15px]" >
-                <div class="flex  flex-col max-[450px]:w-full ">
+            <div class="flex w-[540px] items-center justify-between mb-[30px] max-[1420px]:w-full max-[910px]:flex-col max-[1420px]:gap-[15px]" >
+                <div class="flex  flex-col max-[910px]:w-full ">
                     <p class="text-[#97AFD5] font-[700] leading-[26px] uppercase text-[12px] font-['Roboto'] mb-[10px] max-[1420px]:tracking-[2.2px] max-[1420px]:font-['Inter']">Your Name</p>
                     <input class="placeholder:text-white placeholder:font-['Roboto'] placeholder:font-[400] bg-[#FFFFFF]/10 rounded-[6px] py-[12px] px-[15px] outline-none text-white font-['Roboto'] font-[400] w-[255px]
-                    max-[1420px]:placeholder:font-['Inter'] max-[1420px]:placeholder:text-['14px'] max-[450px]:w-full max-[450px]:font-['Inter']
+                    max-[1420px]:placeholder:font-['Inter'] max-[1420px]:placeholder:text-['14px'] max-[910px]:w-full max-[910px]:font-['Inter']
                     " type="text" placeholder="Introduce yourself" >
                 </div>
 
-                <div class="flex flex-col  max-[450px]:w-full  ">
+                <div class="flex flex-col  max-[910px]:w-full  ">
                     <p class="text-[#97AFD5] font-[700] leading-[26px] uppercase text-[12px] font-['Roboto'] mb-[10px] max-[1420px]:tracking-[2.2px] max-[1420px]:font-['Inter']">Your Email</p>
                     <input class=" placeholder:text-white placeholder:font-['Roboto'] placeholder:font-[400] bg-[#FFFFFF]/10 rounded-[6px] py-[12px] px-[15px] outline-none text-white font-['Roboto'] font-[400] w-[255px]
                     max-[1420px]:placeholder:font-['Inter'] max-[1420px]:placeholder:text-['14px'] max-[1420px]:w-full max-[1420px]:font-['Inter']
@@ -61,14 +61,14 @@
                 </div>
             </div>
 
-            <div class="flex flex-col w-[540px]  mb-[41px] max-[450px]:w-full">
+            <div class="flex flex-col w-[540px]  mb-[41px] max-[910px]:w-full">
                     <p class="text-[#97AFD5] font-[700] leading-[26px] uppercase text-[12px] font-['Roboto'] mb-[10px] max-[1420px]:tracking-[2.2px] max-[1420px]:font-['Inter']">Your Message</p>
                     <textarea placeholder="Leave your question or comment here" class=" placeholder:text-white placeholder:font-['Roboto'] placeholder:font-[400]  w-[100%] h-[105px] bg-[#FFFFFF]/10 rounded-[6px] py-[10px] px-[15px] outline-none text-white font-['Roboto'] font-[400] resize-none"></textarea>
             </div>
 
             
                 <button class="flex items-center w-[211px] justify-center text-center rounded-[20px] bg-[#EC474E] text-white uppercase text-[16px] font-['Barlow'] font-[700] leading-[26px] mr-[2px] ml-[20px] cursor-pointer
-                max-[450px]:rounded-[8px] max-[450px]:w-full
+                max-[910px]:rounded-[8px] max-[910px]:w-full
                 ">
                     send message
                     <img src="../../assets/icons/Contact/arrow-right.png" alt=""/>
--- components/OurAdvantagesChart/OurAdvantagesChart.vue
+++ components/OurAdvantagesChart/OurAdvantagesChart.vue
@@ -9,7 +9,7 @@
         <GraphTwo v-if="state.active_button === 2"/>
     </div>
 
-    <div class="hidden flex-col items-center w-full mt-[107px] h-[1130px] mx-auto my-0 max-[910px]:flex">
+    <div class="hidden flex-col items-center w-full mt-[107px] h-[1130px] mx-auto my-0 max-[910px]:flex max-[600px]:hidden">
         <h2 class="text-white uppercase text-[22px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[62px] translate-x-[-14px]">our advantages</h2>
         <div class="flex w-[450px] border-b-[4px] gap-[10px] no-scrollbar border-b-[#749DCA] h-[80px] justify-center mb-[61px] relative">
             <button @click="setActiveButton(1)" :class="state.active_button === 1 ? 'button_activeMob' : 'button_unactiveMob'" >investors Comparison </button>
@@ -19,7 +19,7 @@
         <GraphTwoForTablet v-if="state.active_button === 2"/>
     </div>
 
-     <div class="hidden flex-col items-center w-full mt-[107px] max-w-[1400px] h-[1130px] mx-auto my-0 max-[450px]:flex">
+     <div class="hidden flex-col items-center w-full mt-[107px] max-w-[1400px] h-[1130px] mx-auto my-0 max-[600px]:flex">
         <h2 class="text-white uppercase text-[22px] font-[700] font-['Big_Shoulders_Stencil_Text'] mb-[62px] translate-x-[-14px]">our advantages</h2>
         <div class="flex w-screen border-b-[4px] gap-[10px] no-scrollbar border-b-[#749DCA] h-[40px] justify-center mb-[61px] relative">
             <button @click="setActiveButton(1)" :class="state.active_button === 1 ? 'button_activeMob' : 'button_unactiveMob'" >investors Comparison </button>
@@ -108,6 +108,22 @@ $returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
     transform: translateY(0);
 
     height: 80px;
+    }
+
+    @media (max-width:910px) {
+    font-size: 14px;
+    width: 200px;
+    transform: translateY(0);
+
+    height: 80px;
+    }
+
+    @media (max-width:600px) {
+    font-size: 14px;
+    width: 250px;
+    transform: translateY(0);
+
+    height: 40px;
     }
 }
 
