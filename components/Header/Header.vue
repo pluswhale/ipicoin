e47bd291ffe011cbe0f1de34a976eb84f88bfc39
Egor Dultsev <listenmy@yandex.ru>
2023-06-27 11:50:17 +0300

    123456

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -11,20 +11,21 @@ const handleOpenBurger = () => {
 <template>
     <header 
         class="flex w-screen h-[93px] relative justify-center items-center shadow-sm bg-white/5 backdrop-blur-sm z-[1000] 
-        max-[1420px]:fixed max-[1420px]:top-[0] max-[1420px]:left-[0] max-[1420px]:h-[43px] max-[1420px]:pl-[16px] max-[1420px]:pr-[16px] "
+        max-[450px]:fixed max-[450px]:top-[0] max-[450px]:left-[0] max-[450px]:h-[43px] max-[450px]:pl-[16px] max-[450px]:pr-[16px] 
+        max-[1450px]:fixed max-[1450px]:top-[0] max-[1450px]:left-[0] max-[1450px]:h-[72px] max-[1450px]:"
     >
-            <div class="flex w-[1400px] justify-between items-center">
-                <div class="flex items-center  max-[1420px]:h-[20px] max-[1420px]:w-[66px]">
+            <div class="flex w-[1400px] justify-between items-center max-[1450px]:w-[900px]">
+                <div class="flex items-center max-[450px]:h-[20px] max-[450px]:w-[66px] max-[1450px]:h-[30px] max-[1450px]:w-[100px]">
                     <!-- <img  class="max-[1420px]:h-[20px] max-[1420px]:w-[66px]" src="~/assets/images/ipi_logo_1.svg"> -->
-                    <img class="max-h-[43px] ml-[11px] max-[1420px]:ml-[1px] " src="~/assets/images/ipi_logo.svg">
+                    <img class="max-h-[43px] ml-[11px] max-[450]:ml-[1px] " src="~/assets/images/ipi_logo.svg">
                 </div>
-                <nav class="flex justify-between translate-x-[-30px] min-w-[395px] max-[1420px]:hidden ">
+                <nav class="flex justify-between translate-x-[-30px] min-w-[395px] max-[450px]:hidden max-[1450px]:flex max-[1450px]:min-w-[313px] max-[1450px]:translate-x-[15px]">
                     <ul class="flex gap-[1.85rem]">
-                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="#platform">Platform</NuxtLink></li>
-                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="/">Coins</NuxtLink></li>
-                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="/">Roadmap</NuxtLink></li>
-                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="/">FAQ</NuxtLink></li>
-                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow']" to="/">Contacts</NuxtLink></li>
+                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow'] max-[1450px]:text-[12px]" to="#platform">Platform</NuxtLink></li>
+                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow'] max-[1450px]:text-[12px]" to="/">Coins</NuxtLink></li>
+                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow'] max-[1450px]:text-[12px]" to="/">Roadmap</NuxtLink></li>
+                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow'] max-[1450px]:text-[12px]" to="/">FAQ</NuxtLink></li>
+                        <li class="list-none"><NuxtLink class=" text-[15px] text-white font-[500] font-['Barlow'] max-[1450px]:text-[12px]" to="/">Contacts</NuxtLink></li>
                     </ul>
                 </nav>
 
@@ -32,24 +33,30 @@ const handleOpenBurger = () => {
                     class="flex pr-[10px] pl-[10px] rounded-[20px] border-[2px] 
                     items-center h-44px translate-x-[10px] justify-center transition-all border-[#EC474E]  
                     font-['Barlow sans-serif'] hover:bg-[#EC474E]
-                    max-[1420px]:rounded-[8px] 
-                    max-[1420px]:pl-[10px] 
-                    max-[1420px]:pr-[10px] 
-                    max-[1420px]:border-[1px]
-                    max-[1420px]:h-[25px] 
-                    max-[1420px]:p-[0] 
-                    max-[1420px]:translate-x-[-6px]
+                    max-[450px]:rounded-[8px] 
+                    max-[450px]:pl-[10px] 
+                    max-[450px]:pr-[10px] 
+                    max-[450px]:border-[1px]
+                    max-[450px]:h-[25px] 
+                    max-[450px]:p-[0] 
+                    max-[450px]:translate-x-[-6px]
                     button_coin
+
+                    max-[1450px]:rounded-[14px]
+                    max-[1450px]:pl-[8px]
+                    max-[1450px]:pr-[8px]
+                    max-[1450px]:h-[30px]
+                    max-[1450px]:translate-x-[-6px]
                     "
                 >
-                    <span class=" font-[500] text-[16px] uppercase whitespace-nowrap text-white font-['Inter'] mr-[5px] max-[1420px]:text-[10px]">
+                    <span class="font-[500] text-[16px] uppercase whitespace-nowrap text-white font-['Inter'] mr-[5px] max-[450px]:text-[10px]  max-[1450px]:text-[10px]">
                         Follow the ipicoin
                     </span>
-                    <img class="no_hover_arrow max-[1420px]:w-[20px]" src="~/assets/icons/arrow-right.svg" />
-                    <img class="hover_arrow max-[1420px]:w-[20px]" src="~/assets/images/IpiScan/arrow-right.png" />
+                    <img class="no_hover_arrow max-[450px]:w-[20px]" src="~/assets/icons/arrow-right.svg" />
+                    <img class="hover_arrow max-[450px]:w-[20px]" src="~/assets/images/IpiScan/arrow-right.png" />
                 </button>
 
-                <img  @click="handleOpenBurger" src="~/assets/icons/mobile/burger_menu.svg" class="relative z-[6] hidden  max-[1420px]:block"/>
+                <img  @click="handleOpenBurger" src="~/assets/icons/mobile/burger_menu.svg" class="relative z-[6] hidden  max-[450px]:block  max-[1450px]:hidden"/>
                     <div v-if="stateMenu.toOpenBurger" class="flex flex-col items-center absolute w-full left-[0] top-[0px] h-screen bg-[#1C0D30]">
                         <div class="flex flex-col items-center h-[615px]">
 
